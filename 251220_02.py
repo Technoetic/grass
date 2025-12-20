7b744abd000697f04bdf8cca1c9e731fbf19da84
Technoetic <technoetic@github.com>
2025-12-21 00:07:04 +0900

    자동 동기화: 251220_01.py, 251220_02.py, 251220_03.py, 251220_04.py, grass.py (2025-12-21 00:07:04)

--- 251220_02.py
+++
@@ -1,8 +0,0 @@
-x = 10
-y = 3
-
-print("x =", x)
-print("y =", y)
-print("x + y =", x + y)
-print("x - y =", x - y)
-print("테스트: 실시간 동기화 확인")
--- 251220_03.py
+++
@@ -1,7 +0,0 @@
-a = input("정수 a: ")
-b = input("정수 b: ")
-
-a = int(a)
-b = int(b)
-
-print("a + b =", a + b)
