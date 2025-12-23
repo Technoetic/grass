2ea7c0eb7816a9bf556601b3c20c61fbc971c919
Technoetic <technoetic@github.com>
2025-12-24 08:55:30 +0900

    자동 커밋 (2025-12-24 08:55:30)

--- grass.py
+++ grass.py
@@ -1,7 +1,7 @@
 import auto_push
 
-add = 0
-for i in range(1, 11):
-    add = add + i
-
-print(add)
+for i in range(2, 10):
+    for j in range(1, 10):
+        print(i * j, end=" ")
+    print('')
+    
