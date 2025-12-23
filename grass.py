e9d6a03f0653c506731e8bd087273420840e15c1
Technoetic <technoetic@github.com>
2025-12-23 10:12:36 +0900

    자동 커밋 (2025-12-23 10:12:35)

--- grass.py
+++ grass.py
@@ -1,5 +1,16 @@
 import auto_push
 
-a = set([1, 2, 3])
+cnt = 1
+even_sum = 0
+odd_sum = 0
 
-print(a)
+for _ in range(100):  # 100번 반복
+    if cnt % 2 == 0:
+        even_sum += cnt
+    else:
+        odd_sum += cnt
+    cnt += 1
+
+print("cnt =", cnt)          # 스크래치에선 마지막에 101이 됨
+print("짝수 =", even_sum)     # 2+4+...+100
+print("홀수 =", odd_sum)      # 1+3+...+99
