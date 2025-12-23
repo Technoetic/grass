410244aa61d554d4a0b9978e98b976210223b0a1
Technoetic <technoetic@github.com>
2025-12-24 08:25:19 +0900

    자동 커밋 (2025-12-24 08:25:19)

--- grass.py
+++ grass.py
@@ -1,9 +1,12 @@
 import auto_push
 
-treeHit = 0
+coffee = 10
+money = 300
 
-while treeHit < 10:
-    treeHit = treeHit + 1
-    print(f"나무를 {treeHit}번 찍었습니다.")
-    if treeHit == 10:
-        print("나무 넘어갑니다.")
+while money:
+    print("돈을 받았으니 커피를 줍니다.")
+    coffee = coffee - 1
+    print(f"남은 커피의 양은 {coffee}개입니다.")
+    if coffee == 0:
+        print("커피가 다 떨어졌습니다. 판매를 중지합니다.")
+        break
