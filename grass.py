6bef50c04fdabf39f8f273728dba5b1cb90b6a08
Technoetic <technoetic@github.com>
2025-12-25 00:51:12 +0900

    자동 커밋 (2025-12-25 00:51:12)

--- grass.py
+++ grass.py
@@ -1,8 +1,8 @@
 import auto_push
 
-for i in range(5):
-    if i ==3:
-        break
-    print(i)
-else:
-    print("for 문이 정상적으로 종료되었습니다.")
+fruits = ['apple', 'banana', 'cherry']
+
+for i, fruit in enumerate(fruits):
+    print(f"{i}: {fruit}")
+
+    
