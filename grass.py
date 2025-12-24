c8887ca840318fbcfa606d39f1dd16a8500aa0a7
Technoetic <technoetic@github.com>
2025-12-25 00:56:33 +0900

    자동 커밋 (2025-12-25 00:56:33)

--- grass.py
+++ grass.py
@@ -3,5 +3,5 @@ import auto_push
 name = ["전문준", "김민수", "이영희"]
 korean = [90, 85, 95]
 english = [85, 90, 80]
-for name, score in zip(name, korean, english):
+for name, korean, english in zip(name, korean, english):
     print(f"{name}: {korean} {english}")
