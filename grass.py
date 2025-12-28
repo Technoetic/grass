0f8a436f9bdd804f1ff9cf5e63032cd2ee9e2c06
Technoetic <technoetic@github.com>
2025-12-29 08:41:34 +0900

    자동 커밋 (2025-12-29 08:41:34)

--- grass.py
+++ grass.py
@@ -1,8 +1,8 @@
 import auto_push
 
-def create_profile(**info):
-    print("=== 인 적 사 항 ===")
-    for key, value in info.items():
-        print(f"{key}: {value}")
+def mix_func(name, *args, **kwargs):
+    print(f"이름: {name}")
+    print(f"숫자 인수들: {args}")
+    print(f"인적 인수들: {kwargs} ")
 
-create_profile(이름 = "김철수", 나이 = 30, 직업 = "개발자", 취미 = "독서")
+mix_func("전문준", 100, 101, age = 25, city = "서울")
