46ae743d976e54e8753fa4e41efb065cb9be2b8d
Technoetic <technoetic@github.com>
2025-12-29 08:29:29 +0900

    자동 커밋 (2025-12-29 08:29:29)

--- grass.py
+++ grass.py
@@ -1,22 +1,4 @@
 import auto_push
 
-def add_mul(choice, *args):
-    if choice == "add":
-        result = 0
-        for i in args:
-            result = result + i
-    elif choice == "mul":
-        result = 1
-        for i in args:
-            result = result * i
-    return result
-
-
-
-result = add_mul("add", 1, 2, 3, 4, 5)
-
-print(result)
-
-result = add_mul("mul", 1, 2, 3, 4, 5)
-
-print(result)
+def print_kwargs(**kwargs):
+    print(kwargs)
