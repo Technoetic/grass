c09db5cf6f6f04097239c5b655173360db381026
Technoetic <technoetic@github.com>
2025-12-29 08:06:19 +0900

    자동 커밋 (2025-12-29 08:06:19)

--- grass.py
+++ grass.py
@@ -1,7 +1,5 @@
 import auto_push
 
-name = ["전문준", "김민수", "이영희"]
-korean = [90, 85, 95]
-english = [85, 90, 80]
-for name, korean, english in zip(name, korean, english):
-    print(f"{name}: {korean} {english}")
+a = add(3, 4)
+print(a)
+
