29da2c73f731bd65969fd2af6ea3cbb26bb0e60b
Technoetic <technoetic@github.com>
2025-12-30 17:46:36 +0900

    자동 커밋 (2025-12-30 17:46:36)

--- grass.py
+++ grass.py
@@ -1,19 +1,19 @@
 import auto_push
 
-# 점수 리스트에서 90점 이상인 자료만 골라 합계와 평균을 구하라!
+# data 각 행의 합계와 전체 총합을 이중 for문을 사용해 출력해보자.
 
-scores = [70, 60, 55, 75, 95, 90, 80, 80, 85, 100]
+data = [
+    [80, 40, 14, 15, 45, 47, 45, 45, 78, 20],
+    [70, 60, 55, 75, 95, 90, 80, 80, 85, 100],  
+    [20, 28, 40, 70, 70, 20, 20, 70, 70, 80]
+]
 
 total = 0
-count = 0
 
-for i in scores:
-    if i >= 90:
-        total += i
-        count += 1
-
-avg = total/count
-
-print(avg)
+for i in range(len(data)):
+    row_sum = 0
+    for j in range(len(data[i])):
+        row_sum += data[i][j]
+        total   += data[i][j]
+    print(f'data[{i}] {row_sum}')
 print(total)
-print(count)
