ff80a3f7d3fee676dc4da6c24b2ed2a6a4c81b84
Technoetic <technoetic@github.com>
2025-12-31 00:09:01 +0900

    자동 커밋 (2025-12-31 00:09:01)

--- grass.py
+++ grass.py
@@ -1,38 +1,11 @@
 import auto_push
 
-# 다음 2차원 리스트 data가 주어졌을 때, 이중 for문을 사용하여 아래 내용을 출력해보세요.
+# 이 문자열을 / 기준으로 분리(split)한 뒤, 리스트 txt에 append(추가) 하여 저장하시오.
 
-# 구현할 내용
-# data[0], data[1], data[2] 각각의 최댓값과 최솟값 출력
-# data 전체에서의 최댓값과 최솟값 출력
+pumMok = '노트북/모니터/키보드'
 
-data = [
-    [80, 40, 14, 15, 45, 47, 45, 45, 78, 20],
-    [70, 60, 55, 75, 95, 90, 80, 80, 85, 100],
-    [20, 28, 40, 70, 70, 20, 20, 70, 70, 80]
-]
+txt = []
 
-all_max = data[0][0]
-all_min = data[0][0]
+txt.extend(pumMok.split('/'))
 
-for i in range(len(data)):
-    row_max = data[i][0]
-    row_min = data[i][0]
-
-    for j in range(len(data[i])):
-        v = data[i][j]
-
-        if v > row_max:
-            row_max = v
-        if v < row_min:
-            row_min = v
-        
-        if v > all_max:
-            all_max = v
-        if v < all_min:
-            all_min = v
-
-    print(f'data[{i}] max:{row_max} min:{row_min}')
-
-print(f'max:{all_max} min:{all_min}')
-    
+print(txt)
