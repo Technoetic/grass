17033f58c74fd94235198c4987c6f8068dafd3fe
Technoetic <technoetic@github.com>
2025-12-30 23:43:30 +0900

    자동 커밋 (2025-12-30 23:43:30)

--- grass.py
+++ grass.py
@@ -1,19 +1,36 @@
 import auto_push
 
-# data 각 행의 합계와 전체 총합을 이중 for문을 사용해 출력해보자.
+# 다음 2차원 리스트 data가 주어졌을 때, 이중 for문을 사용하여 아래 내용을 출력해보세요.
+
+# 구현할 내용
+# data[0], data[1], data[2] 각각의 최댓값과 최솟값 출력
+# data 전체에서의 최댓값과 최솟값 출력
 
 data = [
     [80, 40, 14, 15, 45, 47, 45, 45, 78, 20],
-    [70, 60, 55, 75, 95, 90, 80, 80, 85, 100],  
+    [70, 60, 55, 75, 95, 90, 80, 80, 85, 100],
     [20, 28, 40, 70, 70, 20, 20, 70, 70, 80]
 ]
 
-total = 0
+all_max = 0
+all_min = 0
 
 for i in range(len(data)):
-    row_sum = 0
+    row_max = 0
+    row_min = 0
+
     for j in range(len(data[i])):
-        row_sum += data[i][j]
-        total   += data[i][j]
-    print(f'data[{i}] {row_sum}')
-print(total)
+        v = data[i][j]
+
+        if v > row_max:
+            row_max = v
+        if v < row_min:
+            row_min = v
+
+        if v > all_max:
+            all_max = v
+        if v < all_min:
+            all_min = v
+    print(f'data[{i}] max:{row_max} min:{row_min}')
+
+print(f'max:{all_max} min:{all_min}')
